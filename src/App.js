7ef820163853acd6aac4239d3cdfac1527599b4a
Anton Hlushchuk <anton.hlushchuk@wework.com>
2023-09-03 20:36:29 +0300

    moved client side to the parent dir

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import ProductList from './components/ProductList';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <h1>Simple CRUD</h1>
+      </header>
+      <main>
+        <ProductList />
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ProductList.js
@@ -0,0 +1,35 @@
+// src/components/ProductList.js
+
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+function ProductList() {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:3001/api/data')
+            .then((response) => {
+                setProducts(response.data);
+            })
+            .catch((error) => {
+                console.error('Error fetching data:', error);
+            });
+    }, []);
+
+    return (
+        <div>
+            <h2>Product List</h2>
+            <ul>
+                {products.map((product) => (
+                    <li key={product.id}>
+                        <h3>{product.name}</h3>
+                        <p>{product.description}</p>
+                        <p>Price: ${product.price}</p>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export default ProductList;
